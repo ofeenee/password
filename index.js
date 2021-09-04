16c2132b6d5da4621796a16e9aeafeebbcfb3af4
ofeenee <ofeenee@gmail.com>
2021-09-04 18:00:48 +0300

    move to npm setup

---
+++ index.js
@@ -0,0 +1,62 @@
+import bcrypt from 'bcrypt';
+const { hashSync, hash, verify } = bcrypt;
+
+import validator from 'validator';
+const { isStrongPassword } = validator;
+
+
+class Password {
+  #password;
+
+  constructor(password) {
+    try {
+      if (isStrongPassword(password)) {
+        this.#password = hashSync(password);
+      }
+      else {
+        throw new Error('Password value is invalid.');
+      }
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  static validate(password) {
+    try {
+      if (!password || typeof password !== 'string' || !isStrongPassword(password)) {
+        throw new Error('Password value is invalid.');
+      }
+      else {
+        return true;
+      }
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async update(password) {
+    try {
+      const match = await verify(this.#password, password);
+      if (isStrongPassword(password) && !match) {
+        this.#password = await hash(password);
+      }
+      else if (match) {
+        throw new Error('update is not necessary.');
+      }
+      else {
+        throw new Error('password value is invalid.');
+      }
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  get() {
+    return this.#password;
+  }
+}
+
+export default Password;
