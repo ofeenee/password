90d935046323ea032bc3bb7e60f1578461176d91
ofeenee <ofeenee@gmail.com>
2021-09-12 00:36:43 +0300

    rewrite Password  class to prototype

--- Password.js
+++ Password.js
@@ -1,76 +1,100 @@
-import isStrongPassword from 'validator/lib/isStrongPassword.js';
-import {hash, verify} from 'argon2';
+import validator from 'validator';
+const {isStrongPassword} = validator;
+
+import {hash as argon2Hash, verify as argon2Verify} from 'argon2';
 
 
-class Password {
-  #password;
+function Password() {
+  try {
+    if (new.target === undefined) return new Password();
+    let hash = null;
 
-  constructor() {
-    try {
-      this.#password = null;
-    }
-    catch (error) {
-      throw error;
-    }
-  }
-
-  async set(password) {
-    try {
-      const valid = isStrongPassword(password);
-
-      if (!valid) throw new Error('password value is invalid.');
-
-      if (this.#password) {
-        const match = await verify(this.#password, password);
-        if (match) return this.#password;
+    async function comparePassword(string) {
+      try {
+        if (validatePassword(string)) {
+          const match = await argon2Verify(hash, string);
+          return match;
+        }
+        else return false
       }
-
-      this.#password = await hash(password);
-      return this.#password;
-
-    }
-    catch (error) {
-      throw error;
-    }
-  }
-
-  get() {
-    if (this.#password) return this.#password;
-    else return null;
-  }
-
-  async compare(password) {
-    try {
-      if (!this.#password) return false;
-      if (!password || typeof password !== 'string' || !isStrongPassword(password)) return false;
-
-      const match = await verify(this.#password, password);
-      return match;
-    }
-    catch (error) {
-      throw error;
-    }
-  }
-
-  static validate(password) {
-    try {
-      if (!password || typeof password !== 'string' || !isStrongPassword(password)) {
-        return false;
-      }
-      else {
-        return true;
+      catch (error) {
+        throw error;
       }
     }
-    catch (error) {
-      throw error;
-    }
+
+    return Object.defineProperties(this, {
+      validate: {
+        value: validatePassword,
+        enumerable: true
+      },
+      setPassword: {
+        value: async function createHash(string) {
+          try {
+            // if string value is invalid (not strong password)
+            if (!validatePassword(string) && !isHash(string)) throw new Error('password value is invalid.');
+
+            // if value is the same as current
+            if (hash && isHash(hash)) {
+              if (string === hash || await comparePassword(string)) return hash;
+            }
+
+            // assign string as is to hash,
+            // or hash plain string then assign it to hash
+            if (isHash(string)) {
+              hash = string;
+              return hash;
+            }
+            else {
+              hash = await argon2Hash(string);
+              return hash;
+            }
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      getPassword: {
+        value: function getHash() {
+          try {
+            if (isHash(hash)) return hash;
+            else return null;
+          } catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      compare: {
+        value: comparePassword,
+        enumerable: true
+      },
+    });
   }
-  static isHash(string) {
-    const regex = new RegExp(/^\$argon2i\$v=19\$m=4096,t=3,p=1\$[0-z+/]{22,22}\$[0-z/+]{43,43}$/);
-    const test = regex.test(string);
-    if (test) return true;
-    else return false;
+  catch (error) {
+    throw error;
   }
 }
 
 export default Password;
+
+
+
+// HELPER FUNCTIONS
+function validatePassword(string) {
+  try {
+    if (typeof string !== 'string' || !string) throw new Error('value is invalid.');
+    return isStrongPassword(string);
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+function isHash(string) {
+  const regex = new RegExp(/^\$argon2i\$v=19\$m=4096,t=3,p=1\$[0-z+/]{22,22}\$[0-z/+]{43,43}$/);
+  const test = regex.test(string);
+  if (test) return true;
+  else return false;
+}
